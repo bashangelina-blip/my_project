1cbb392d5c2e69429d5ae5224d24ab1fbd8967dc
Anhelina Basharova <bondarangelina37@gmail.com>
2026-03-24 18:47:08 +0500

    variables

--- src/main/java/com/homework/TaskOne.java
+++ src/main/java/com/homework/TaskOne.java
@@ -3,5 +3,19 @@ package com.homework;
 public class TaskOne {
     public static void main(String[] args) {
         System.out.println("Hello, world");
+        byte b = 10;
+        int a = 13 + b;
+        short c = 200;
+        int d = 60000;
+        long myLongValue = 456789L;
+        float myFloatValue = 23.56F;
+        double myDoubleValue = 54.56;
+        System.out.println("b = "+b);
+        System.out.println("a = "+a);
+        System.out.println("c = "+c);
+        System.out.println("d = "+d);
+        System.out.println("long = "+myLongValue);
+        System.out.println("float = "+myFloatValue);
+        System.out.println("double = "+myDoubleValue);
     }
 }
