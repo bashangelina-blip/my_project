fa124d79054199f534640af464bf13becd2b1a05
Anhelina Basharova <bondarangelina37@gmail.com>
2026-03-25 11:37:53 +0500

    if-else conditions

---
+++ src/main/java/com/homework/TaskTwoConditions.java
@@ -0,0 +1,20 @@
+package com.homework;
+
+public class TaskTwoConditions {
+    public static void main(String[] args) {
+        int testScore = 65;
+        System.out.println("You've got");
+        if (testScore >= 90) {
+            System.out.println("an excellent");
+        } else if (testScore >= 75) {
+            System.out.println("a good");
+
+        } else if (testScore >= 60) {
+            System.out.println("an okay");
+
+        } else {
+            System.out.println("a bad");
+        }
+        System.out.println("mark");
+    }
+}
