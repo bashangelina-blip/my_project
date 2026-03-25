f562dd0451b73aa6a677a9e487bbb0b3d2ddd2ac
Anhelina Basharova <bondarangelina37@gmail.com>
2026-03-25 12:42:09 +0500

    for cycle

---
+++ src/main/java/com/homework/TaskTwoForCycle.java
@@ -0,0 +1,16 @@
+package com.homework;
+
+public class TaskTwoForCycle {
+    public static void main(String[] args) {
+        int factorialLimit = 10;
+        int factorial = 1;
+        for (int i = 1; i < factorialLimit; i++) {
+            factorial *= i;
+            System.out.println("Factorial = " + factorial);
+        }
+        System.out.println("Final factorial value =" + factorial);
+
+    }
+
+
+}
