44648269bc8eb200627e687a11fdbfbf0048140c
Anhelina Basharova <bondarangelina37@gmail.com>
2026-03-25 18:13:37 +0500

    methods

---
+++ src/main/java/com/homework/TaskTwoMethods.java
@@ -0,0 +1,24 @@
+package com.homework;
+
+public class TaskTwoMethods {
+    public static void main(String[] args) {
+        sayHello("Mike");
+        squareRect(4, 7);
+        System.out.println("Square: " + rectSquare(5, 9));
+
+    }
+
+    static void sayHello(String name) {
+        System.out.println("Hello, " + name);
+    }
+
+    static void squareRect(int x, int y) {
+        int square = x * y;
+        System.out.println("Rectangle square: " + square);
+    }
+
+    static int rectSquare(int x, int y) {
+        return x * y;
+    }
+
+}
