3746b069f6211eed51c0885a0e18adcb4d592b65
Anhelina Basharova <bondarangelina37@gmail.com>
2026-03-26 20:05:53 +0500

    static methods

--- src/main/java/com/homework/OOP/ClassesAndObjects/Dog.java
+++ src/main/java/com/homework/OOP/ClassesAndObjects/Dog.java
@@ -1,6 +1,46 @@
 package com.homework.OOP.ClassesAndObjects;
 
 public class Dog {
+
+    private static int dogsCount;
+
+
+    public String getSize() {
+        return size;
+    }
+
+
+    public void setSize(String size) {
+        if (size.equalsIgnoreCase("Big") ||
+                size.equalsIgnoreCase("Average") ||
+                size.equalsIgnoreCase("Small")) {
+            this.size = size;
+        } else {
+            System.out.println("Size should be one of these: Big, Average, Small");
+        }
+
+    }
+
+    public void bite() {
+        if (dogsCount > 2) {
+            System.out.println("Dogs are biting you");
+        } else {
+            bark();
+        }
+
+    }
+
+    public void bark() {
+        if (size.equalsIgnoreCase("Big")) {
+            System.out.println("Woof-woof");
+            ;
+        } else if (size.equalsIgnoreCase("Average")) {
+            System.out.println("Raff-raff");
+        } else {
+            System.out.println("Tiaf-tiaf");
+        }
+    }
+
     public int getPaws() {
         return paws;
     }
@@ -9,6 +49,7 @@ public class Dog {
         if (paws == 4) {
             this.paws = paws;
         } else {
+            this.paws = 4;
             System.out.println("User tried to set " + paws + " paws");
         }
 
@@ -22,7 +63,7 @@ public class Dog {
         if (tail == 1) {
             this.tail = tail;
         } else {
-            this.paws = paws;
+            this.tail = 1;
             System.out.println("User tried to set " + tail + " tails");
         }
     }
@@ -46,6 +87,16 @@ public class Dog {
     private int paws = 4;
     private int tail = 1;
     private String name;
-    public String breed;
+    private String breed;
+    private String size;
+
+    public Dog() {
+        dogsCount++;
+        System.out.println("Dogs count: " + getDogsCount());
+    }
+
+    public static int getDogsCount() {
+        return dogsCount;
+    }
 
 }
--- src/main/java/com/homework/OOP/ClassesAndObjects/Main.java
+++ src/main/java/com/homework/OOP/ClassesAndObjects/Main.java
@@ -1,22 +1,42 @@
 package com.homework.OOP.ClassesAndObjects;
 
+
 public class Main {
     public static void main(String[] args) {
-        Dog labrador = new Dog();
-        labrador.setPaws(3);
+        System.out.println("Dogs count: " + Dog.getDogsCount());
 
+
+        Dog labrador = new Dog();
+        labrador.setPaws(4);
         labrador.setName("Charlie");
         labrador.setBreed("Labrador");
+        labrador.setSize("Average");
+        labrador.bite();
+
 
         Dog shepherd = new Dog();
         shepherd.setPaws(4);
         shepherd.setTail(1);
         shepherd.setBreed("Shepherd");
         shepherd.setName("Kali");
+        shepherd.setSize("Big");
+        shepherd.bite();
 
-        System.out.println("Lab's name is "+labrador.getName());
-        System.out.println("Shepherd's name is "+ shepherd.getName());
-        System.out.println("Lab's tail is "+labrador.getTail());
+        Dog chihuahua = new Dog();
+        chihuahua.setPaws(4);
+        chihuahua.setTail(1);
+        chihuahua.setBreed("Chihuhua");
+        chihuahua.setName("Lola");
+        chihuahua.setSize("Small");
+        chihuahua.bite();
+
+
+
+        System.out.println("Lab's name is " + labrador.getName());
+        System.out.println("Shepherd's name is " + chihuahua.getName());
+        System.out.println("Lab's tail is " + labrador.getTail());
+
+
 
 
     }
