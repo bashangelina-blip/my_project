dadf94ccc7345ee86c6bc0427f101aae72a2e45d
Anhelina Basharova <bondarangelina37@gmail.com>
2026-04-06 13:53:21 +0500

    changes

--- src/main/java/com/homework/oop/composition/Bank_main.java
+++ src/main/java/com/homework/oop/composition/Bank_main.java
@@ -1,4 +1,22 @@
 package com.homework.oop.composition;
 
 public class Bank_main {
+    public static void main(String[] args) {
+        BankSystem bank = new BankSystem();
+
+        User user1 = bank.createUser("Anna", "Mostypan", 28);
+        User user2 = bank.createUser("Lina", "Bondar", 29);
+
+        Account a1 = new Account(1000);
+        CreditAccount a2 = new CreditAccount(1000, 200);
+
+        user1.addAccount(a1);
+        user2.addAccount(a2);
+        bank.printAllUsers();
+        System.out.println();
+
+        bank.transfer(a1,a2,300);
+        bank.printAllUsers();
+
+    }
 }
